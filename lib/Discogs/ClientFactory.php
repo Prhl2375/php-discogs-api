11080ab8eecfd07e1baf0d0355f67aba0b3376dd
Serhii <dahhipie25@gmail.com>
2024-03-28 13:09:21 +0200

    client factory fix

--- lib/Discogs/ClientFactory.php
+++ lib/Discogs/ClientFactory.php
@@ -12,34 +12,32 @@ namespace Discogs;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Command\Guzzle\Description;
-use GuzzleHttp\Command\Guzzle\GuzzleClient;
 
-class ClientFactory
+final class ClientFactory
 {
-    public static function factory(array $config = [])
+    public static function factory(array $config = []): DiscogsClient
     {
         $defaultConfig = [
-            'defaults' => [
-                'headers' => ['User-Agent' => 'php-discogs-api/1.0.0 +https://github.com/ricbra/php-discogs-api'],
-                'auth' => 'oauth'
-            ],
+            'headers' => ['User-Agent' => 'php-discogs-api/2.0.0 +https://github.com/calliostro/php-discogs-api'],
+            'auth' => 'oauth',
         ];
 
         $client = new Client(self::mergeRecursive($defaultConfig, $config));
         $service = include __DIR__ . '/../../resources/service.php';
         $description = new Description($service);
 
-        return new GuzzleClient($client, $description);
+        return new DiscogsClient($client, $description);
     }
 
-    public static function &mergeRecursive(array &$array1, &$array2 = null)
+    private static function &mergeRecursive(array $array1, $array2 = null): array
     {
         $merged = $array1;
 
         if (is_array($array2)) {
             foreach ($array2 as $key => $val) {
-                if (is_array($array2[$key])) {
-                    $merged[$key] = isset($merged[$key]) && is_array($merged[$key]) ? self::mergeRecursive($merged[$key], $array2[$key]) : $array2[$key];
+                if (is_array($val)) {
+                    $merged[$key] = isset($merged[$key]) && is_array($merged[$key]) ?
+                        self::mergeRecursive($merged[$key], $val) : $val;
                 } else {
                     $merged[$key] = $val;
                 }
